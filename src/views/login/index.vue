befca21658860f425866616206af197cf148fd8d
zhantao <tzhang6@i9i8.com>
2020-04-20 13:12:49 +0800

    ok

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Welcome</h3>
+        <h3 class="title">蜻蜓系统X3.0-后台管理</h3>
       </div>
 
       <el-form-item prop="username">
